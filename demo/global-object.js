c38089592153f169416c388298ae290babad8f20
GurifYuanin <1109983335@qq.com>
2018-05-26 22:59:07 +0800

    add process.html

--- demo/global-object.js
+++ demo/global-object.js
@@ -1,28 +1,31 @@
-var path = require('path');
-// console.log('__dirname: ' + __dirname);
-// console.log('__filename: ' + __filename);
-// console.dir(process);
-console.log(path.basename('a/b/c'));
-console.log(path.basename('a/b/c.html'));
-console.log(path.dirname('a/b/c'));
-console.log(path.dirname('a/b/c.html'));
-console.log(path.extname('a/b/c'));
-console.log(path.extname('a/b/c.html'));
-console.log(path.format({
-	root: '/e',
-	dir: '/a/b',
-	base: 'c.html',
-	name: 'd',
-	ext: '.xhtml'
-}));
-console.log(path.parse('a/b/c.html'));
-console.log(path.isAbsolute('C:/'));
-console.log(path.isAbsolute('/'));
-console.log(path.isAbsolute('a/b/c.html'));
-console.log(path.join('a/b', 'c.html'));
-console.log(path.join('', 'c.html'));
-console.log(path.normalize('a/../a/../a/b/c.html'));
-console.log(path.normalize('a/b/././c.html'));
-console.log(path.resolve('/a/b', 'c.html'));
-console.log(path.resolve('/a', '/b', 'c.html'));
-console.log(path.resolve('a/b', 'c.html'));
+// var path = require('path');
+// // console.log('__dirname: ' + __dirname);
+// // console.log('__filename: ' + __filename);
+// // console.dir(process);
+// console.log(path.basename('a/b/c'));
+// console.log(path.basename('a/b/c.html'));
+// console.log(path.dirname('a/b/c'));
+// console.log(path.dirname('a/b/c.html'));
+// console.log(path.extname('a/b/c'));
+// console.log(path.extname('a/b/c.html'));
+// console.log(path.format({
+// 	root: '/e',
+// 	dir: '/a/b',
+// 	base: 'c.html',
+// 	name: 'd',
+// 	ext: '.xhtml'
+// }));
+// console.log(path.parse('a/b/c.html'));
+// console.log(path.isAbsolute('C:/'));
+// console.log(path.isAbsolute('/'));
+// console.log(path.isAbsolute('a/b/c.html'));
+// console.log(path.join('a/b', 'c.html'));
+// console.log(path.join('', 'c.html'));
+// console.log(path.normalize('a/../a/../a/b/c.html'));
+// console.log(path.normalize('a/b/././c.html'));
+// console.log(path.resolve('/a/b', 'c.html'));
+// console.log(path.resolve('/a', '/b', 'c.html'));
+// console.log(path.resolve('a/b', 'c.html'));
+
+// console.log(process.execArgv);
+console.log(process.argv);
