65b2faa3b2ef1e6bd9bd9b34e99c333c73a86ae9
gurifyuanin <1109983335@qq.com>
2018-10-12 23:05:29 +0800

    phongmaterial

--- js/three-init.js
+++ js/three-init.js
@@ -1,3 +1,15 @@
+function addOnceClickEventById(id, fn) {
+	var el = document.getElementById(id);
+	el.addEventListener('click', function() {
+		el.parentNode.removeChild(el);
+		fn();
+	});
+}
+
+function value2color(str) {
+	return new THREE.Color(Number.parseInt(str.substring(1), 16));
+}
+
 function initRenderer( el, options ) {
 	if (!el) {
 		console.error('no element to mount');
@@ -32,14 +44,15 @@ function initScene( objects ) {
 	}
 	return scene;
 }
-
+function initBox() {
+	return new THREE.BoxGeometry(1, 1, 1);
+}
 function initMesh(geometry, material) {
 	return new THREE.Mesh(geometry, material);
 }
 
 function initAmbientLight(color) {
 	var light = new THREE.AmbientLight(color || 0xffffff);
-	light.position.set(100, 100, 100);
 	return light;
 }
 
@@ -47,3 +60,4 @@ function initPointLight(color) {
 	var light = new THREE.PointLight(color || 0xffffff, 1, 100);
 	return light;
 }
+
